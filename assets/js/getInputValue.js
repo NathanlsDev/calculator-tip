f9b61cc491aa7e9930d1bffbc6313175c2509c80
nathan <nathanls.dev@gmail.com>
2022-06-30 20:04:33 -0500

    get values of inputs

---
+++ assets/js/getInputValue.js
@@ -0,0 +1,20 @@
+let bill = document.querySelector('#bill');
+let quantity = document.querySelector('#numPerson');
+let customValue = document.querySelector('#customValue');
+
+bill.addEventListener('input', getInputValue);
+quantity.addEventListener('input', getInputValue);
+customValue.addEventListener('input', getInputValue);
+
+function getInputValue () {
+  billValue = Number(bill.value);
+  console.log(billValue);
+
+  numPerson = Number(quantity.value);
+  console.log(numPerson);
+  
+  custom = Number(customValue.value);
+  console.log(custom);
+
+  math(billValue, numPerson, custom);
+}
