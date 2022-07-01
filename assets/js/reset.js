ff9ba44b9f6ca6b547909865a9b5db4da7b76efe
nathan <nathanls.dev@gmail.com>
2022-06-30 20:40:21 -0500

    new script for reset app

---
+++ assets/js/reset.js
@@ -0,0 +1,18 @@
+let erase = document.querySelector('#reset');
+erase.addEventListener('click', () =>{
+  let bill = document.querySelector('#bill');
+  let person = document.querySelector('#numPerson');
+  let amountP = document.querySelector("#total1");
+  let totalP = document.querySelector("#total2");
+
+  bill.value = '';
+  person.value = '';
+  amountP.textContent = `$0.00`;
+  totalP.textContent = `$0.00`;
+});
+
+function clean (){
+  let cleanner = document.querySelector('#customValue');
+  cleanner.value = '';
+}
+
